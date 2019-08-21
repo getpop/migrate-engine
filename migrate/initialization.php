b3443255982e9e5a7b2a520ecc5564b30c106c39
Leo <losoviz@gmail.com>
2019-08-21 17:45:00 +0800

    Moved code from Engine to ComponentModel

--- migrate/initialization.php
+++ migrate/initialization.php
@@ -8,12 +8,6 @@ class Initialization
         load_plugin_textdomain('pop-engine', false, dirname(plugin_basename(__FILE__)).'/languages');
 
         /**
-         * Load the Server first, so we can access class ServerUtils
-         * And its required library first
-         */
-        include_once 'server/load.php';
-
-        /**
          * Load the Contracts
          */
         include_once 'contracts/load.php';
