242b3e1153e6f8530199231563f3d6ee19ec6399
Leo <losoviz@gmail.com>
2019-09-07 13:34:04 +0800

    Fixed empty path

--- migrate/library/dataload/datastructure-formatters/graphql.php
+++ migrate/library/dataload/datastructure-formatters/graphql.php
@@ -9,19 +9,24 @@ class DataStructureFormatter_GraphQL extends DataStructureFormatter_MirrorQuery
     {
         return GD_DATALOAD_DATASTRUCTURE_GRAPHQL;
     }
-    
+
     public function getFormattedData($data)
     {
         // GraphQL places the queried data under entries 'data' => query => results
         // Replicate this structure. Because we don't have a query name here, replace it with the queried URL path, which is known to the client
+        $path = \PoP\ComponentModel\Utils::getURLPath();
+        // If there is no path, it is the single point of entry (homepage => root)
+        if (!$path) {
+            $path = '/';
+        }
         return [
             'data' => [
-                \PoP\ComponentModel\Utils::getURLPath() => parent::getFormattedData($data),
+                $path => parent::getFormattedData($data),
             ],
         ];
     }
 }
-    
+
 /**
  * Initialize
  */
