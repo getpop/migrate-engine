6c7dc1b4d8856b64087ff94a82d06c4d172dddf0
Leo <losoviz@gmail.com>
2019-09-07 16:07:42 +0800

    Typed function

--- migrate/contracts/cms-apis/cms-functionapi-interface.php
+++ migrate/contracts/cms-apis/cms-functionapi-interface.php
@@ -7,7 +7,7 @@ interface FunctionAPI
     public function redirect($url);
     public function getVersion();
     public function getHomeURL();
-    public function getSiteURL();
+    public function getSiteURL(): string;
     public function isError($object);
     public function getContentDir();
     public function getContentURL();
