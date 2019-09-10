34dd2516af48f9f67068ee6f5e8ea923ea537322
Leo <losoviz@gmail.com>
2019-09-10 18:51:43 +0800

    Added errors and deprecations to GraphQL

--- migrate/library/dataload/datastructure-formatters/graphql.php
+++ migrate/library/dataload/datastructure-formatters/graphql.php
@@ -1,6 +1,8 @@
 <?php
 namespace PoP\Engine\Impl;
 
+use PoP\Translation\Facades\TranslationAPIFacade;
+
 define('GD_DATALOAD_DATASTRUCTURE_GRAPHQL', 'graphql');
 
 class DataStructureFormatter_GraphQL extends DataStructureFormatter_MirrorQuery
@@ -19,11 +21,47 @@ class DataStructureFormatter_GraphQL extends DataStructureFormatter_MirrorQuery
         if (!$path) {
             $path = '/';
         }
-        return [
+        $ret = [
             'data' => [
                 $path => parent::getFormattedData($data),
             ],
         ];
+        // Add errors
+        $errors = [];
+        if ($data['dbErrors']) {
+
+        }
+        if ($data['schemaErrors']) {
+            $errors = array_merge(
+                $errors,
+                $this->reformatSchemaEntries($data['schemaErrors'])
+            );
+        }
+        if ($errors) {
+            $ret['errors'] = $errors;
+        }
+
+        // Add deprecations
+        if ($data['schemaDeprecations']) {
+            $ret['deprecations'] = $this->reformatSchemaEntries($data['schemaDeprecations']);
+        }
+        return $ret;
+    }
+
+    protected function reformatSchemaEntries($entries)
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $ret = [];
+        foreach ($entries as $field => $message) {
+            $ret[] = [
+                'message' => sprintf(
+                    $translationAPI->__('Field \'%s\': %s', 'pop-api-graphql'),
+                    $field,
+                    $message
+                ),
+            ];
+        }
+        return $ret;
     }
 }
 
