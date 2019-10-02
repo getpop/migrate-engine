790576fa7170ba174affd6037b3b53554e79c7e4
Leo <losoviz@gmail.com>
2019-10-02 19:23:12 +0800

    "if-fields" was converted to "if", self evaluated

--- migrate/library/dataload/directives/filter-ids-satisfying-condition-trait.php
+++ migrate/library/dataload/directives/filter-ids-satisfying-condition-trait.php
@@ -8,15 +8,11 @@ trait FilterIDsSatisfyingConditionTrait
     {
         // Check the condition field. If it is satisfied, then skip those fields
         $idsSatisfyingCondition = [];
-        $field = $this->directiveArgs['if-field'];
-        $fieldOutputKey = FieldUtils::getFieldOutputKey($field);
         foreach (array_keys($idsDataFields) as $id) {
             $resultItem = $resultIDItems[$id];
-            $value = $fieldResolver->resolveValue($resultItem, $field);
-            if (\PoP\ComponentModel\GeneralUtils::isError($value)) {
-                $error = $value;
-                $dbErrors[(string)$id][$fieldOutputKey][] = $error->getErrorMessage();
-            } elseif ($value) {
+            // Extract the directiveArguments, to be evaluated on the field as fieldArgs
+            $fieldArgs = FieldUtils::extractFieldArgumentsForResultItem($fieldResolver, $resultItem, $this->directive);
+            if ($fieldArgs['if']) {
                 $idsSatisfyingCondition[] = $id;
             }
         }
