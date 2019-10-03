e6e12cbdafb8f6ccb3d2ca208e9e57e54e6c3313
Leo <losoviz@gmail.com>
2019-10-03 21:40:38 +0800

    Fixed bug

--- migrate/library/dataload/fieldprocessors/load.php
+++ migrate/library/dataload/fieldprocessors/load.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once 'fieldprocessor-operators-bas.php';
+require_once 'fieldprocessor-operators-base.php';
 require_once 'fieldprocessor-operators.php';
 require_once 'fieldprocessor-site-hook-trait.php';
