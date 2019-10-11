29d47cd8444e0df6a6918beb9cfff4f29f1951d7
Leo <losoviz@gmail.com>
2019-10-11 22:21:35 +0800

    Moved classes to API

--- migrate/library/dataload/directives/load.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once 'filter-ids-satisfying-condition-trait.php';
-require_once 'directiveresolver-include.php';
-require_once 'directiveresolver-skip.php';
--- migrate/library/dataload/load.php
+++ migrate/library/dataload/load.php
@@ -3,4 +3,3 @@
 require_once 'datastructure-formatters/load.php';
 require_once 'queryhandlers/load.php';
 require_once 'fieldprocessors/load.php';
-require_once 'directives/load.php';
