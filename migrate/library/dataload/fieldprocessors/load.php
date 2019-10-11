f21f8924bb15eb21679f92cf19ce12f218492d58
Leo <losoviz@gmail.com>
2019-10-11 22:54:37 +0800

    Moved classes

--- migrate/library/dataload/fieldprocessors/load.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once 'fieldprocessor-operators-base.php';
-require_once 'fieldprocessor-operators.php';
-require_once 'fieldprocessor-site-hook-trait.php';
--- migrate/library/dataload/load.php
+++ migrate/library/dataload/load.php
@@ -2,4 +2,3 @@
 
 require_once 'datastructure-formatters/load.php';
 require_once 'queryhandlers/load.php';
-require_once 'fieldprocessors/load.php';
