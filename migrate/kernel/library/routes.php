9eba082dbc3fd3e666b288b681010be18a66d7c6
Leo <losoviz@gmail.com>
2019-10-12 17:56:13 +0800

    Changed namespace from Engine to Definitions

--- migrate/kernel/library/routes.php
+++ migrate/kernel/library/routes.php
@@ -3,8 +3,8 @@
 // public const POP_ROUTE_DESCRIPTION = 'description';
 // public const POP_ROUTE_AUTHORS = 'authors';
 if (!defined('POP_ROUTE_DESCRIPTION')) {
-    define('POP_ROUTE_DESCRIPTION', \PoP\Engine\DefinitionUtils::getUniqueDefinition('description', POP_DEFINITIONGROUP_ROUTES));
+    define('POP_ROUTE_DESCRIPTION', \PoP\Definitions\DefinitionUtils::getUniqueDefinition('description', POP_DEFINITIONGROUP_ROUTES));
 }
 if (!defined('POP_ROUTE_AUTHORS')) {
-    define('POP_ROUTE_AUTHORS', \PoP\Engine\DefinitionUtils::getUniqueDefinition('authors', POP_DEFINITIONGROUP_ROUTES));
+    define('POP_ROUTE_AUTHORS', \PoP\Definitions\DefinitionUtils::getUniqueDefinition('authors', POP_DEFINITIONGROUP_ROUTES));
 }
