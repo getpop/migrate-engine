7bbe01e619a9c9d6c892587631f457942b2f16e9
Leo <losoviz@gmail.com>
2019-10-21 17:20:15 +0800

    Removed function `hash`

--- migrate/contracts/cms-apis/cms-helperapi-interface.php
+++ migrate/contracts/cms-apis/cms-helperapi-interface.php
@@ -6,5 +6,4 @@ interface HelperAPI
     public function addQueryArgs(array $key_values, string $url): string;
     public function removeQueryArgs(array $keys, string $url): string;
     public function maybeAddTrailingSlash(string $text): string;
-    public function hash($data): string;
 }
