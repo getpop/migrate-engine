6c19e1cff210e8dae8e828aba677a16e7fa71148
Leo <losoviz@gmail.com>
2019-10-23 13:46:47 +0800

    Migrated definitions

--- migrate/kernel/library/routes.php
+++ migrate/kernel/library/routes.php
@@ -1,10 +1,12 @@
 <?php
+use PoP\Definitions\Facades\DefinitionManagerFacade;
+$definitionManager = DefinitionManagerFacade::getInstance();
 
 // public const POP_ROUTE_DESCRIPTION = 'description';
 // public const POP_ROUTE_AUTHORS = 'authors';
 if (!defined('POP_ROUTE_DESCRIPTION')) {
-    define('POP_ROUTE_DESCRIPTION', \PoP\Definitions\DefinitionUtils::getUniqueDefinition('description', POP_DEFINITIONGROUP_ROUTES));
+    define('POP_ROUTE_DESCRIPTION', $definitionManager->getUniqueDefinition('description', POP_DEFINITIONGROUP_ROUTES));
 }
 if (!defined('POP_ROUTE_AUTHORS')) {
-    define('POP_ROUTE_AUTHORS', \PoP\Definitions\DefinitionUtils::getUniqueDefinition('authors', POP_DEFINITIONGROUP_ROUTES));
+    define('POP_ROUTE_AUTHORS', $definitionManager->getUniqueDefinition('authors', POP_DEFINITIONGROUP_ROUTES));
 }
