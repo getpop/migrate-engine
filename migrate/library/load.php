dad313cc939189a283b1df74af2eaf567e6d5e42
Leo <losoviz@gmail.com>
2019-10-24 22:53:09 +0800

    Migrated ModuleRouting

--- migrate/library/load.php
+++ migrate/library/load.php
@@ -4,5 +4,4 @@ require_once 'functions/load.php';
 require_once 'dataload/load.php';
 require_once 'settingsprocessors/load.php';
 require_once 'processors/load.php';
-require_once 'routemoduleprocessors/load.php';
 require_once 'filterinputs/load.php';
