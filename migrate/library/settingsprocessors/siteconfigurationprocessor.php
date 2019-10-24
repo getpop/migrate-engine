1179988f8e3828efd4eb9a860791814813e7424f
Leo <losoviz@gmail.com>
2019-10-24 22:09:16 +0800

    Migrated ModuleRouting

--- migrate/library/settingsprocessors/siteconfigurationprocessor.php
+++ migrate/library/settingsprocessors/siteconfigurationprocessor.php
@@ -1,12 +1,14 @@
 <?php
 namespace PoP\Engine\Settings\Impl;
+use PoP\ModuleRouting\Facades\RouteModuleProcessorManagerFacade;
+use PoP\ModuleRouting\ModuleRoutingGroups;
 
 class PageModuleSiteConfigurationProcessor extends \PoP\ComponentModel\Settings\SiteConfigurationProcessorBase
 {
     public function getEntryModule(): ?array
     {
-        $pop_module_routemoduleprocessor_manager = \PoP\ModuleRouting\RouteModuleProcessorManagerFactory::getInstance();
-        return $pop_module_routemoduleprocessor_manager->getRouteModuleByMostAllmatchingVarsProperties(POP_PAGEMODULEGROUP_ENTRYMODULE);
+        $pop_module_routemoduleprocessor_manager = RouteModuleProcessorManagerFacade::getInstance();
+        return $pop_module_routemoduleprocessor_manager->getRouteModuleByMostAllmatchingVarsProperties(ModuleRoutingGroups::ENTRYMODULE);
     }
 }
 
