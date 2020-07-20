22269f652e923b5c39137d486a9ae03dd7226071
Leonardo Losoviz <leo@getpop.org>
2020-07-20 23:52:22 +0800

    Fixed signature

--- migrate/kernel/forminputs/order.php
+++ migrate/kernel/forminputs/order.php
@@ -3,14 +3,14 @@ namespace PoP\Engine;
 
 class GD_FormInput_Order extends GD_FormInput_Select
 {
-    public function getValue($output = false)
+    public function getValue(?array $source = null)
     {
-        if ($value = parent::getValue($output)) {
+        if ($value = parent::getValue($source)) {
 
             // There must be exactly 2 elements: orderby|order
             $elems = explode('|', $value);
             if (count($elems) == 2) {
-                
+
                 return array('orderby' => $elems[0], 'order' => $elems[1]);
             }
         }
