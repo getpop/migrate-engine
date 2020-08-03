3c5a39c22ae3a744417440c68c6304a00e0d1e72
Leonardo Losoviz <leo@getpop.org>
2020-08-03 15:05:28 +0800

    Migrated function `fullUrl`

--- migrate/library/functions/utils.php
+++ migrate/library/functions/utils.php
@@ -16,33 +16,6 @@ function removeDomain($url)
     return substr($url, strlen(getDomain($url)));
 }
 
-/**
- * Return the requested full URL
- *
- * @param boolean $useHostRequestedByClient If true, get the host from user-provided HTTP_HOST, otherwise from the server-defined SERVER_NAME
- * @return string
- */
-function fullUrl(bool $useHostRequestedByClient = false): string
-{
-    $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
-    $sp = strtolower($_SERVER["SERVER_PROTOCOL"]);
-    $protocol = substr($sp, 0, strpos($sp, "/")) . $s;
-    /**
-     * The default ports (80 for HTTP and 443 for HTTPS) must be ignored
-     */
-    $isDefaultPort = $s ? $_SERVER["SERVER_PORT"] == "443" : $_SERVER["SERVER_PORT"] == "80";
-    $port = $isDefaultPort ? "" : (":" . $_SERVER["SERVER_PORT"]);
-    /**
-     * If accessing from Nginx, the server_name might point to localhost
-     * instead of the actual server domain. So provide the change to use
-     * the user-requested host
-     *
-     * @see https://stackoverflow.com/questions/2297403/what-is-the-difference-between-http-host-and-server-name-in-php
-     */
-    $host = $useHostRequestedByClient ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
-    return $protocol . "://" . $host . $port . $_SERVER['REQUEST_URI'];
-}
-
 function arrayFlatten(array $array, bool $firstLevelOnly = false)
 {
     $return = array();
