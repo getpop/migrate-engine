18d51bf1c03180c6f2a8d5ec1247db5576f92e57
Leonardo Losoviz <leo@getpop.org>
2020-12-17 18:49:05 +0800

    Formatted code

--- migrate/kernel/forminputs/order.php
+++ migrate/kernel/forminputs/order.php
@@ -6,11 +6,9 @@ class GD_FormInput_Order extends GD_FormInput_Select
     public function getValue(?array $source = null)
     {
         if ($value = parent::getValue($source)) {
-
             // There must be exactly 2 elements: orderby|order
             $elems = explode('|', $value);
             if (count($elems) == 2) {
-
                 return array('orderby' => $elems[0], 'order' => $elems[1]);
             }
         }
